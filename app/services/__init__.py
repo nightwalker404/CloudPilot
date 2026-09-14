765d1a6ed0ec74f4556f13dd645e47fb9007891d
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-09-15 03:27:36 +0330

    add service

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,3 +1,3 @@
-from .llm_response import extract_tool_call_from_content
+from .llm_response import extract_tool_call_from_content, llm_service
 
-__all__ = ["extract_tool_call_from_content"]
+__all__ = ["extract_tool_call_from_content", "llm_service"]
