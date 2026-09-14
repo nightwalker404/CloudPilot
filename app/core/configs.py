0b08370604886e7080566c901434e3b1c95f87f0
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-09-14 16:54:52 +0330

    add: comment and data base variabels

--- app/core/configs.py
+++ app/core/configs.py
@@ -5,11 +5,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    ### App Configuration ###
     app_name: str = Field(..., validation_alias="APP_NAME")
     debug_mode: bool = Field(default=False, validation_alias="DEBUG_MODE")
-    database_url: str = Field(..., validation_alias="DATABASE_URL")
+
+    ### AI Configuration ###
     model: str = Field(..., validation_alias="MODEL")
     llm_base_url: str = Field(..., validation_alias="LLM_BASE_URL")
+
+    ### MongoDB Configuration ###
+    mongo_uri: str = Field(..., validation_alias="MONGO_URI")
+    mongo_initdb_root_username: str = Field(..., validation_alias="MONGO_INITDB_ROOT_USERNAME")
+    mongo_initdb_root_password: str = Field(..., validation_alias="MONGO_INITDB_ROOT_PASSWORD")
 
     model_config = SettingsConfigDict(
         env_file=".env",
