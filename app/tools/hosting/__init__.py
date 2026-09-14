43d030d22753e249186ba36beb5840e017a86c3a
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-09-14 17:24:36 +0330

    update: via the next moving files we updates pathes

--- app/tools/hosting/__init__.py
+++ app/tools/hosting/__init__.py
@@ -1,4 +1,3 @@
-from app.tools.hosting.functions import create_vm
-from app.tools.hosting.schema import VM_SCHEMA
+from app.tools.hosting.vm import create_vm, VM_SCHEMA
 
 __all__ = ["create_vm", "VM_SCHEMA"]
---
+++ app/tools/hosting/vm/__init__.py
@@ -0,0 +1,2 @@
+from .functions import create_vm
+from .schema import VM_SCHEMA
