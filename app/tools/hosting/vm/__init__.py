4646c6335a7d555027fa0dc266f8565798bd5600
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-09-15 16:34:05 +0330

    init imports

--- app/tools/hosting/vm/__init__.py
+++ app/tools/hosting/vm/__init__.py
@@ -1,5 +1,19 @@
-from .functions import create_vm
-from .schema import VM_SCHEMA
+from .functions import (
+    create_vm, list_vms, get_vm, delete_vm, start_vm, stop_vm,
+    _validate_vm_params, _sanitize_vm_name
+)
+from .schema import (
+    CREATE_VM_SCHEMA, LIST_VMS_SCHEMA, GET_VM_SCHEMA,
+    DELETE_VM_SCHEMA, START_VM_SCHEMA, STOP_VM_SCHEMA,
+    VM_SCHEMA, TOOLS_SCHEMA
+)
 from .cloud_init.loader import load_cloud_init_yaml
 
-__all__ = ["create_vm", "VM_SCHEMA", "load_cloud_init_yaml"]
+__all__ = [
+    "create_vm", "list_vms", "get_vm", "delete_vm", "start_vm", "stop_vm",
+    "_validate_vm_params", "_sanitize_vm_name",
+    "CREATE_VM_SCHEMA", "LIST_VMS_SCHEMA", "GET_VM_SCHEMA",
+    "DELETE_VM_SCHEMA", "START_VM_SCHEMA", "STOP_VM_SCHEMA",
+    "VM_SCHEMA", "TOOLS_SCHEMA",
+    "load_cloud_init_yaml"
+]
---
+++ app/tools/websearch/__init__.py
@@ -0,0 +1,5 @@
+from .functions import web_search
+from .schema import WEB_SEARCH_SCHEMA
+from .registry import TOOLS_MAP, TOOLS_SCHEMA
+
+__all__ = ["web_search", "WEB_SEARCH_SCHEMA", "TOOLS_MAP", "TOOLS_SCHEMA"]
