4bc6d29907c585c4e6ed6e069cf38c48f2d1c056
nobody1889 <nobodyisherefornow1977@gmail.com>
2026-09-15 16:33:33 +0330

    add new functions into host registry

--- app/tools/hosting/registry.py
+++ app/tools/hosting/registry.py
@@ -1,11 +1,24 @@
-from .vm import create_vm, VM_SCHEMA
+from .vm import (
+    create_vm, list_vms, get_vm, delete_vm, start_vm, stop_vm,
+    CREATE_VM_SCHEMA, LIST_VMS_SCHEMA, GET_VM_SCHEMA, DELETE_VM_SCHEMA, START_VM_SCHEMA, STOP_VM_SCHEMA
+)
 
 # Combine them
 TOOLS_MAP = {
     "create_vm": create_vm,
+    "list_vms": list_vms,
+    "get_vm": get_vm,
+    "delete_vm": delete_vm,
+    "start_vm": start_vm,
+    "stop_vm": stop_vm,
 }
 
 # Combine schemas
 TOOLS_SCHEMA = [
-    VM_SCHEMA,
+    CREATE_VM_SCHEMA,
+    LIST_VMS_SCHEMA,
+    GET_VM_SCHEMA,
+    DELETE_VM_SCHEMA,
+    START_VM_SCHEMA,
+    STOP_VM_SCHEMA,
 ]
